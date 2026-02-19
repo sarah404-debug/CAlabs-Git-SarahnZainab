c1f9868472aae0c87fc7d7cc6f461848bc829280
Zainab Bhutto <zainab.bhutto5678@gmail.com>
2026-02-19 10:38:29 +0500

    lab 5

---
+++ CA_LAB5/CA_LAB5.srcs/sources_1/new/countdown_fsm.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/19/2026 10:08:06 AM
+// Design Name: 
+// Module Name: countdown_fsm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module countdown_fsm(
+
+    );
+endmodule
